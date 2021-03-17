698e8871064f2fad633897149ca28b94cfdfa4cc
Andrew Wasielewski <andrewwasielewski@Andrews-MBP.fios-router.home>
2021-03-17 12:33:58 -0400

    can view followers

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-#TEST TEST TEST
+import instaloader
+
+
+L = instaloader.Instaloader()
+UN = input("Enter username: ")
+PW = input("Enter password: ")
+
+L.login(UN,PW)
+
+profile = instaloader.Profile.from_username(L.context, str(UN))
+
+followers = []
+n = 0
+
+for followee in profile.get_followers():
+	followers.append(followee.username)
+	file = open(str(UN)+ "_followers.txt", "a+")
+	file.write(followers[n])
+	file.write("\n")
+	file.close()
+	print(followers[n])
+	n = n + 1
+
+
