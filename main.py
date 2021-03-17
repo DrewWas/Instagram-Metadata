8b999d180896c9ead5ee87eaf4203428f2827442
Andrew Wasielewski <andrewwasielewski@Andrews-MBP.fios-router.home>
2021-03-17 14:05:29 -0400

    find whos not following you done

--- main.py
+++ main.py
@@ -12,9 +12,32 @@ profile = instaloader.Profile.from_username(L.context, str(UN))
 followers = []
 n = 0
 
+print("0% done")
+
 for followee in profile.get_followers():
 	followers.append(followee.username)
-	n = n + 1
 
-print(followers) 
-print(len(followers))
+
+following_1 = []
+q = 0
+
+print("50% done")
+
+for following in profile.get_followees():
+	following_1.append(following.username)
+	q = q + 1
+
+
+print("75% done")
+
+not_back = []
+
+for i in following_1:
+	if i in following_1 and i not in followers:
+		not_back.append(i)
+
+for i in not_back:
+	print(i)
+print(len(not_back))
+
+
