db95c2487d38dd73129b4103d0463bbca31641e6
Andrew Wasielewski <andrewwasielewski@Andrews-MBP.fios-router.home>
2021-03-18 12:41:18 -0400

    you can now include/exclude verified users in the search for who doesnt follow you back (bc obv most verified accounts wouldnt follow you back

--- main.py
+++ main.py
@@ -4,40 +4,71 @@ import instaloader
 L = instaloader.Instaloader()
 UN = input("Enter username: ")
 PW = input("Enter password: ")
+includeVerified = False
+tt = input("Would you like to include verified users in this search? (y/n): ")
+
+if tt == "y":
+	includeVerified = True
+else:
+	includeVerified = False
+
 
 L.login(UN,PW)
 
 profile = instaloader.Profile.from_username(L.context, str(UN))
 
 followers = []
-n = 0
+following_1 = []
+not_back = []
 
 print("0% done")
 
-for followee in profile.get_followers():
-	followers.append(followee.username)
+def all():
+
+	for followee in profile.get_followers():
+		followers.append(followee.username)
 
 
-following_1 = []
-q = 0
 
-print("50% done")
+	print("50% done")
 
-for following in profile.get_followees():
-	following_1.append(following.username)
-	q = q + 1
+	for following in profile.get_followees():
+		following_1.append(following.username)
 
 
-print("75% done")
+	print("75% done")
 
-not_back = []
 
-for i in following_1:
-	if i in following_1 and i not in followers:
-		not_back.append(i)
+	for i in following_1:
+		if i in following_1 and i not in followers:
+			not_back.append(i)
 
-for i in not_back:
-	print(i)
-print(len(not_back))
+
+
+
+if includeVerified == True:
+	all()
+	for i in not_back:
+		print(i)
+	print(len(not_back))	
+
+else:
+	for followee in profile.get_followers():
+		followers.append(followee.username)
+	print("50% done")
+	for following in profile.get_followees():
+		if following.is_verified == False:
+			following_1.append(following.username)
+	print("75% done")
+	for i in following_1:
+		if i in following_1 and i not in followers:
+			not_back.append(i)
+
+	for i in not_back:
+		print(i)
+	print(len(not_back))
+	
+
+
 
 
